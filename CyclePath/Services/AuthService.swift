58fab3060a6b214c3c96945ea9eaf4cef724471d
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-09-26 09:49:20 +0200

    [ADD](Firebase)[!M] Firebase|Added & Configured

---
+++ CyclePath/Services/AuthService.swift
@@ -0,0 +1,14 @@
+//
+//  AuthService.swift
+//  CyclePath
+//
+//  Created by Guillaume Loulier on 26/09/2017.
+//  Copyright © 2017 Guillaume Loulier. All rights reserved.
+//
+
+import Foundation
+
+class AuthService
+{
+    
+}
